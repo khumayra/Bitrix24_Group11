60f3b5facb1879cd87e14ad9dc0afb608fe37a9f
sevimzini <sevim.zini@gmail.com>
2020-04-12 16:23:20 -0400

    "Login Test"

---
+++ src/test/java/automation/tests/LoginTest.java
@@ -0,0 +1,18 @@
+package automation.tests;
+
+import automation.pages.login.LoginPage;
+import org.testng.annotations.Test;
+
+public class LoginTest extends AbstractTestBase {
+
+    LoginPage loginPage = new LoginPage();
+
+    @Test
+    public void loginTest1() {
+
+
+        loginPage.login();
+        loginPage.navigateTo("Activity Stream");
+
+    }
+}

143e1e488e74209ea9e2ceefafe149489fb9834e
sevimzini <sevim.zini@gmail.com>
2020-04-12 16:28:26 -0400

    "Login Test"

--- src/test/java/automation/pages/AbstractPageBase.java
+++ src/test/java/automation/pages/AbstractPageBase.java
@@ -15,25 +15,29 @@ import java.util.List;
 
 
 /**
-     * This class will be extended by page classes
-     * Ant common webelements/locators can be stored here
-     * Since navigation menu doesn't belong to particular page
-     * We cannot really create a dedicated page class to store
-     * elements from that menu
-     */
-    public abstract class AbstractPageBase {
-        protected WebDriver driver = Driver.getDriver();
-        protected WebDriverWait wait = new WebDriverWait(driver, 20);
+ * This class will be extended by page classes
+ * Ant common webelements/locators can be stored here
+ * Since navigation menu doesn't belong to particular page
+ * We cannot really create a dedicated page class to store
+ * elements from that menu
+ */
+public abstract class AbstractPageBase {
+    protected WebDriver driver = Driver.getDriver();
+    protected WebDriverWait wait = new WebDriverWait(driver, 20);
 
-     public void navigateTo (String menuName){
+    public void navigateTo(String menuName) {
 
-     String name = "//span [@class = 'menu-item-link-text' and contains (text (), '"+menuName+"')]";
-         WebElement menuElement = driver.findElement(By.xpath(name));
+        String name = "//span [@class = 'menu-item-link-text' and contains (text (), '" + menuName + "')]";
+        WebElement menuElement = driver.findElement(By.xpath(name));
+
+      /* One shot way:
+         WebElement menuElement = Driver.getDriver().findElement(By.xpath("//span [@class = 'menu-item-link-text' and contains (text (), '"+menuName+"')]"));
+      */
+
         wait.until(ExpectedConditions.visibilityOf(menuElement));
         wait.until(ExpectedConditions.elementToBeClickable(menuElement));
         menuElement.click();
 
-
-     }
     }
+}
 

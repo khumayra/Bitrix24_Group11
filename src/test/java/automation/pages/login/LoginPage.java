f10f486e9585606c959dd0b7c86d2dee62cbed11
khumayra <khumayra@gmail.com>
2020-04-12 04:04:41 -0400

    login page

--- src/test/java/automation/pages/login/LoginPage.java
+++ src/test/java/automation/pages/login/LoginPage.java
@@ -1,4 +1,21 @@
 package automation.pages.login;
 
+import automation.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    @FindBy (name = "USER_LOGIN")
+    public WebElement userName;
+
+    @FindBy  (name="USER_PASSWORD")
+    public WebElement password;
+
+    @FindBy (className = "login-btn")
+    public WebElement loginBtn;
+
 }

939aec63133fbc6b2a50c577b5f540173fbf7cf4
khumayra <khumayra@gmail.com>
2020-04-13 22:20:37 -0400

    JS Executor added

--- src/test/java/automation/tests/task/TaskTests.java
+++ src/test/java/automation/tests/task/TaskTests.java
@@ -5,7 +5,9 @@ import automation.pages.task.TaskPage;
 import automation.tests.AbstractTestBase;
 import automation.utilities.BrowserUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,10 +15,9 @@ import java.util.List;
 
 public class TaskTests extends AbstractTestBase {
     private By textEditorBarBy = By.xpath("//div[@id='bx-html-editor-tlbr-lifefeed_task_form']//span");
-    private By streamTimeBy = By.className("feed-time");
 
     @Test
-    public void verifyHighPriorityChkBx (){
+    public void verifyHighPriorityChkBx () throws Exception{
         test = report.createTest("Selecting high priority checkbox make task top priority task");
         String titleValue = "Very Important Task";
         String descriptionValue = "Make it High Priority";
@@ -26,16 +27,29 @@ public class TaskTests extends AbstractTestBase {
         taskPage.navigateTo("Task");
 
         //filling out task form
-        taskPage.enterTaskTitle(titleValue);
-        taskPage.enterTaskDescription(descriptionValue);
+//        taskPage.enterTaskTitle(titleValue);
+//        taskPage.enterTaskDescription(descriptionValue);
+//        taskPage.selectHighPriority();
+//        taskPage.clickOnSave();
+//        taskPage.pressViewTaskBtnPopUp();
+//        driver.switchTo().activeElement();
+//        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("//div[@class='side-panel-content-container']//iframe")));
+//        String actual = driver.findElement(By.id("task-detail-important-button")).getAttribute("class");
+//        String expected = "task-info-panel-important mutable";
+//        Assert.assertEquals(actual,expected);
+//
+        Thread.sleep(4000);
+        WebElement switchLabel = driver.findElement(By.xpath("//label[.='High Priority']"));
+        String colorRGB = ((JavascriptExecutor)driver)
+                .executeScript("return window.getComputedStyle(arguments[0], '::after').getPropertyValue('background-position');",switchLabel).toString();
+        System.out.println(colorRGB);
         taskPage.selectHighPriority();
-        taskPage.clickOnSave();
-        taskPage.pressViewTaskBtnPopUp();
-        driver.switchTo().activeElement();
-        driver.switchTo().frame(driver.findElement(By.xpath("//div[@class='side-panel-content-container']//iframe")));
-        String actual = driver.findElement(By.cssSelector("#task-detail-important-button")).getAttribute("class");
-        String expected = "task-info-panel-important mutable";
-        Assert.assertEquals(actual,expected);
+        colorRGB = ((JavascriptExecutor)driver)
+                .executeScript("return window.getComputedStyle(arguments[0], '::after').getPropertyValue('background-position');",switchLabel).toString();
+        System.out.println(colorRGB);
+        //String expected = "task-info-panel-important mutable";
+        //Assert.assertEquals(actual,expected);
+
     }
 
     @Test

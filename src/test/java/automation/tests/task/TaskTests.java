35137e5910910bce55836814e323a27f17a804cd
Mehmet.Durkan <2234396>
2020-04-13 22:39:41 -0500

    Checkbox pseudo element after:: tested

--- src/test/java/automation/tests/task/TaskTests.java
+++ src/test/java/automation/tests/task/TaskTests.java
@@ -5,6 +5,7 @@ import automation.pages.task.TaskPage;
 import automation.tests.AbstractTestBase;
 import automation.utilities.BrowserUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -16,26 +17,22 @@ public class TaskTests extends AbstractTestBase {
     private By streamTimeBy = By.className("feed-time");
 
     @Test
-    public void verifyHighPriorityChkBx (){
+    public void verifyHighPriorityChkBx () {
         test = report.createTest("Selecting high priority checkbox make task top priority task");
-        String titleValue = "Very Important Task";
-        String descriptionValue = "Make it High Priority";
         LoginPage loginPage = new LoginPage();
         loginPage.login();
         TaskPage taskPage = new TaskPage();
         taskPage.navigateTo("Task");
-
-        //filling out task form
-        taskPage.enterTaskTitle(titleValue);
-        taskPage.enterTaskDescription(descriptionValue);
+        BrowserUtils.wait(4);
+        WebElement switchLabel = driver.findElement(By.xpath("//label[.='High Priority']"));
+        String actualBackgroundPositionBeforeChecked = ((JavascriptExecutor)driver)
+                .executeScript("return window.getComputedStyle(arguments[0], '::after').getPropertyValue('background-position');",switchLabel).toString();
+        Assert.assertEquals(actualBackgroundPositionBeforeChecked,"0px -103px");
         taskPage.selectHighPriority();
-        taskPage.clickOnSave();
-        taskPage.pressViewTaskBtnPopUp();
-        driver.switchTo().activeElement();
-        driver.switchTo().frame(driver.findElement(By.xpath("//div[@class='side-panel-content-container']//iframe")));
-        String actual = driver.findElement(By.cssSelector("#task-detail-important-button")).getAttribute("class");
-        String expected = "task-info-panel-important mutable";
-        Assert.assertEquals(actual,expected);
+        String actualBackgroundPositionAfterChecked = ((JavascriptExecutor)driver)
+                .executeScript("return window.getComputedStyle(arguments[0], '::after').getPropertyValue('background-position');",switchLabel).toString();
+        Assert.assertEquals(actualBackgroundPositionAfterChecked,"0px -85px");
+
     }
 
     @Test

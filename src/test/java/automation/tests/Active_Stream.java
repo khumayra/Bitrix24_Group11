59d186812aebbd1a2c344db13893ab1de61c615d
NuriArikan <nuri@cornelldecor.com>
2020-04-16 16:59:27 -0400

    test script

---
+++ src/test/java/automation/tests/Active_Stream.java
@@ -0,0 +1,41 @@
+package automation.tests;
+
+import automation.pages.login.LoginPage;
+import automation.utilities.BrowserUtils;
+import automation.utilities.Driver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Active_Stream extends AbstractTestBase {
+
+    static WebDriver driver = Driver.getDriver();
+
+    LoginPage loginPage = new LoginPage();
+
+    @Test
+    public void activeStream() {
+        test = report.createTest("Verify Activity Stream and Save Filter ");
+        loginPage.login();
+        BrowserUtils.wait(3);
+        driver.findElement(By.id("LIVEFEED_search")).click();
+        BrowserUtils.wait(2);
+        driver.findElement(By.xpath("//*[@id=\"popup-window-content-LIVEFEED_search_container\"]/div/div/div[2]/div[1]/div[1]/div[1]")).click();
+        BrowserUtils.wait(2);
+        driver.findElement(By.name("CREATED_BY_ID_label")).sendKeys("hr46@cybertekschool.com");
+        BrowserUtils.wait(2);
+        driver.findElement(By.name("TO_label")).sendKeys("All employees");
+        BrowserUtils.wait(2);
+        driver.findElement(By.xpath("//*[@id=\"popup-window-content-LIVEFEED_search_container\"]/div/div/div[3]/div[2]/div/button")).click();
+        BrowserUtils.wait(4);
+        driver.findElement(By.id("LIVEFEED_search")).click();
+        BrowserUtils.wait(2);
+        driver.findElement(By.xpath("//*[@id=\"popup-window-content-LIVEFEED_search_container\"]/div/div/div[3]/div[1]/span[1]")).click();
+        BrowserUtils.wait(3);
+        driver.findElement(By.xpath("//*[@id=\"popup-window-content-LIVEFEED_search_container\"]/div/div/div[3]/div[3]/div/span[1]")).click();
+        BrowserUtils.wait(3);
+    }
+}
--- src/test/java/automation/utilities/Driver.java
+++ src/test/java/automation/utilities/Driver.java
@@ -35,12 +35,12 @@ public class Driver {
             switch (browser) {
                 case "chrome":
                     // Headless mode make executions faster it does everything except file uploading
-                    WebDriverManager.chromedriver().setup();
+                    WebDriverManager.chromedriver().version("81").setup();
                     driver = new ChromeDriver();
                     break;
                 case "chromeheadless":
                     //to run chrome without interface
-                    WebDriverManager.chromedriver().setup();
+                    WebDriverManager.chromedriver().version("81").setup();
                     ChromeOptions options = new ChromeOptions();
                     options.setHeadless(true);      //to run browser without interface
               //      options.addArguments("--start-maximized");
